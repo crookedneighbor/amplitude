cb886f17b5a015302f7a56370c8eee81db9ab71f
Blade Barringer <blade@crookedneighbor.com>
2015-06-23 21:20:45 -0500

    set user and device id

--- test/initialize.test.js
+++ test/initialize.test.js
@@ -7,6 +7,30 @@ describe('initialization', () => {
 
       expect(amplitude.token).to.eql('token');
     });
+
+    it('sets session_user_id to undefined when no id specified', () => {
+      let amplitude = new Amplitude('token');
+
+      expect(amplitude.session_user_id).to.eql(undefined);
+    });
+
+    it('sets session_device_id to undefined when no id specified', () => {
+      let amplitude = new Amplitude('token');
+
+      expect(amplitude.session_device_id).to.eql(undefined);
+    });
+
+    it('looks up and sets session_user_id', () => {
+      let amplitude = new Amplitude('token', { user_id: 'db_user_id' });
+
+      expect(amplitude.session_user_id).to.eql('db_user_id');
+    });
+
+    it('looks up and sets session_device_id', () => {
+      let amplitude = new Amplitude('token', { device_id: 'db_device_id' });
+
+      expect(amplitude.session_device_id).to.eql('db_device_id');
+    });
   });
 
   context('without api token', () => {
