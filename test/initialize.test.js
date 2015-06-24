297d0f75c785c8a90dc6ef5c0d312256c8fd8817
Blade Barringer <blade@crookedneighbor.com>
2015-06-23 19:53:32 -0500

    Clarified initialization tests

--- test/initialize.test.js
+++ test/initialize.test.js
@@ -1,17 +1,21 @@
 import { Amplitude } from '../lib/amplitude';
 
 describe('initialization', () => {
-  it('is ininitalized with an api token', () => {
-    let amplitude = new Amplitude('token');
+  context('with api token', () => {
+    it('is ininitalized', () => {
+      let amplitude = new Amplitude('token');
 
-    expect(amplitude.token).to.eql('token');
+      expect(amplitude.token).to.eql('token');
+    });
   });
 
-  it('throws an error if initialized without a token', () => {
-    sinon.spy(Amplitude);
+  context('without api token', () => {
+    it('throws an error', () => {
+      sinon.spy(Amplitude);
 
-    expect(() => {
-      let amplitude = new Amplitude();
-    }).to.throw(/No token provided/);
+      expect(() => {
+        let amplitude = new Amplitude();
+      }).to.throw(/No token provided/);
+    });
   });
 });
