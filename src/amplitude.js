e6a421712e5d38b29b3d9524dbe15df59ae8258b
Blade Barringer <blade@crookedneighbor.com>
2016-08-03 13:37:20 -0500

    refactor: Remove q dependency
    closes #14

--- src/amplitude.js
+++ src/amplitude.js
@@ -1,4 +1,3 @@
-import Q from 'q';
 import request from 'superagent';
 
 class Amplitude {
@@ -10,34 +9,27 @@ class Amplitude {
     this.session_device_id = options.device_id;
   }
 
-  track(data) {
+  track (data) {
     data.user_id = data.user_id || this.session_user_id ;
     data.device_id = data.device_id || this.session_device_id ;
-    return Q.Promise((resolve, reject) => {
-      _postToApi(this.token, data, resolve, reject);
-    });
+
+    return this._postToApi(data);
+  }
+
+  _postToApi (data) {
+    return request
+      .post('https://api.amplitude.com/httpapi')
+      .query({
+        api_key: this.token,
+        event: JSON.stringify(data)
+      })
+      .set('Accept', 'application/json')
+      .then(res => res.body);
   }
 }
 
 function _checkForToken(token) {
   if (!token) throw 'No token provided';
-}
-
-function _postToApi(token, data, resolve, reject) {
-  request
-    .post('https://api.amplitude.com/httpapi')
-    .query({
-      api_key: token,
-      event: JSON.stringify(data)
-    })
-    .set('Accept', 'application/json')
-    .end((err, res) => {
-      if (err) {
-        var name = data.user_id || data.device_id;
-        return reject(err);
-      }
-      resolve(res.body);
-  });
 }
 
 export default Amplitude;
