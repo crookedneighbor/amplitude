bc52655817cb252c7e59937cf1a2f33129365e5c
Blade Barringer <blade@crookedneighbor.com>
2016-08-04 17:25:57 -0500

    refactor: Remove dependency on babel and gulp
    closes #15

---
+++ tasks/generate-contributors-list.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const fs = require('fs')
+const PKG = require('../package.json')
+const README_LOCATION = __dirname + '/../README.md'
+
+function cleanReadme(file) {
+  let lines = file.split('\n')
+  let lastLine = lines.indexOf('------>')
+  let readme = lines.slice(0, lastLine + 1).join('\n')
+
+  return readme
+}
+
+function getContributorText() {
+  let text = '\n\n## Contributors\n\n'
+  let contributors = Object.keys(PKG.contributors).map(con => PKG.contributors[con])
+
+  contributors.forEach((contributor) => {
+    text += `+ [${contributor.name}](${contributor.url})\n`
+  });
+
+  return text
+}
+
+let readmeFile = fs.readFileSync(README_LOCATION, { encoding: 'utf8' })
+let readme = cleanReadme(readmeFile)
+
+let contributorText = getContributorText()
+
+readme += contributorText
+
+fs.writeFileSync(README_LOCATION, readme)
+
